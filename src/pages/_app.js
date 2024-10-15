efe1e037ab5e278fcea62b160c6ffd69409a9dc9
Your Name <you@example.com>
2024-10-15 16:39:51 +0200

    Implement SEO by having metatags and pre-rendered data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,22 @@
-// src/pages/_app.js
 import { AuthProvider } from '../context/AuthContext';
 import Head from 'next/head';
 import '../styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
+  const { title, description, keywords, image } = pageProps;
+
   return (
     <AuthProvider>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Your Name or Company" />
-        <meta name="description" content="This is a default description for the entire site." />
-        <meta name="keywords" content="your, default, keywords, here" />
+        <meta name="description" content={description || 'This is a default description for the entire site.'} />
+        <meta name="keywords" content={keywords || 'your, default, keywords, here'} />
         <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
-        {/* You can add more meta tags here as needed */}
+        <title>{title || 'E-commerce'}</title>
+        {image && <meta property="og:image" content={image} />}
       </Head>
       <Component {...pageProps} />
     </AuthProvider>
